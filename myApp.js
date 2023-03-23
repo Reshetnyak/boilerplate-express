9bd2cee5628b8f62765bc73ffde336c436aeda52
Denys Reshetniak <reshetnjak@gmail.com>
2023-03-23 17:34:50 +0100

    Add logger middleware

--- myApp.js
+++ myApp.js
@@ -2,6 +2,11 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 
+app.use((req, res, next) => {
+    console.log(`${req.method} ${req.path} - ${req.ip}`);
+    next();
+});
+
 app.use('/public', express.static(__dirname + '/public'));
 
 app.get('/', (req, res) => {
