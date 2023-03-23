f5f2b6b7edb462a51d7d8f4eeee53cf3e47b2a1e
Denys Reshetniak <reshetnjak@gmail.com>
2023-03-23 19:16:07 +0100

    Use body parser

--- myApp.js
+++ myApp.js
@@ -1,6 +1,9 @@
 require('dotenv').config();
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({extended: false}));
 
 app.use((req, res, next) => {
     console.log(`${req.method} ${req.path} - ${req.ip}`);
